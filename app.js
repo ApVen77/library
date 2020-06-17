a78429577f8e2b73c7727ac4717b5ddf1a4dec5e
RubinaldoMingos <rubinaldomingos@gmail.com>
2020-06-17 08:24:58 +0200

    update

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const chalk = require('chalk');
-const debug = require('debug')('app');
-const morgan = require('morgan');
-const path = require('path');
+const express = require("express");
+const chalk = require("chalk");
+const debug = require("debug")("app");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dis')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname, "/public")));
+app.use(
+  "/css",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/js",
+  express.static(path.join(__dirname, "/node_modules/jquery/dis"))
+);
+
+app.set("views", "src/views");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  res.render("index");
 });
 app.listen(port, () => {
   debug(`listen on port ${chalk.green(port)}`);
